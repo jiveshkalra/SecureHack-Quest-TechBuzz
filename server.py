bae2065cb4d59c8f7937fc5011929923de9930ba
Jivesh Kalra <jiveshkalra4@gmail.com>
2024-07-25 20:55:32 +0530

    blog page done

--- server.py
+++ server.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template
-from mysql_db_config import get_db
-
+from mysql_db_config import get_db 
+ 
 mydb = get_db()
-app = Flask(__name__)
-
-
+app = Flask(__name__) 
+ 
 @app.route("/")
 def index(): 
     mycursor = mydb.cursor()   
@@ -12,6 +11,21 @@ def index():
     blogs_data = mycursor.fetchall() 
     return render_template('index.html', blogs_data=blogs_data)
 
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+@app.route("/signup")
+def signup():
+    return render_template('signup.html')
+
+@app.route("/blogs/<blog_url>")
+def blog(blog_url): 
+    mycursor = mydb.cursor()
+    # SELECT * FROM `blogs` WHERE `blog_url` = "typography" 
+    mycursor.execute(f"SELECT * FROM blogs WHERE blog_url = '{blog_url}'")
+    blog_data = mycursor.fetchone()   
+    return render_template('blogs.html', blog_data=blog_data ) 
 
 if __name__ == "__main__":
     app.run(debug=True)
--- temp.py
+++ temp.py
@@ -1,20 +1,19 @@
-import mysql.connector
-from datetime import datetime
+from mysql_db_config import get_db
 
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  password="12345678",  
-  database="secure_hack"
-)
- 
-#  INSERT INTO `blogs` (` `title`, `short description`, `content`, `author_id`, `author_name`, `comments_id`, `img_link`, `blog_url`, `timestamp`) VALUES  (title,short_desc,content,author_id, author_name,comments_id,img_link, blog_url, current_timestamp());
+mydb = get_db()
+#  INSERT INTO `blogs` (` `title`, `short description`, `content`, `author_id`, `author_name`, `img_link`, `blog_url`, `timestamp`) VALUES  (title,short_desc,content,author_id, author_name,comments_id,img_link, blog_url, current_timestamp());
 
 mycursor = mydb.cursor()
 
-mycursor.execute("SELECT * FROM blogs") 
-myresult = mycursor.fetchall()
-
-for i in myresult:
-    print(i[9]) 
- 
+blog_url = 'typography'
+mycursor.execute(f"SELECT * FROM blogs WHERE blog_url = '{blog_url}'")
+blog_data = mycursor.fetchone()
+print(blog_data[0]) # Sno
+print(blog_data[1]) # Title 
+print(blog_data[2]) # Short Description
+print(blog_data[3]) # Content
+print(blog_data[4]) # Author ID
+print(blog_data[5]) # Author Name
+print(blog_data[6]) # img_link
+print(blog_data[7]) # blog_url
+print(blog_data[8]) # timestamp
