52f37ff134adf954a752ce6aab4d87dc3020149e
Jivesh Kalra <jiveshkalra4@gmail.com>
2024-07-25 19:45:43 +0530

    automatic blog loading done

--- server.py
+++ server.py
@@ -1,11 +1,34 @@
 from flask import Flask , render_template
+import mysql.connector
+import datetime 
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="12345678",  
+  database="secure_hack"
+)
+ 
 
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    
+    mycursor = mydb.cursor()
+
+    mycursor.execute("SELECT * FROM blogs")
+
+    blogs_data = mycursor.fetchall()
+    # for index,i in enumerate(blogs_data):
+    #     time_stamp = i[9]  
+    #     dt_obj = datetime.fromtimestamp(time_stamp)
+    #     i[10] = dt_obj.strftime("%d %B, %Y")
+    #     # update the new blogs_data with the new timestamp
+        
+     
+    return render_template('index.html',blogs_data=blogs_data)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- temp.py
+++ temp.py
@@ -1,9 +1,20 @@
 import mysql.connector
+from datetime import datetime
 
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
-  password="12345678"
+  password="12345678",  
+  database="secure_hack"
 )
+ 
+#  INSERT INTO `blogs` (` `title`, `short description`, `content`, `author_id`, `author_name`, `comments_id`, `img_link`, `blog_url`, `timestamp`) VALUES  (title,short_desc,content,author_id, author_name,comments_id,img_link, blog_url, current_timestamp());
 
-print(mydb)
+mycursor = mydb.cursor()
+
+mycursor.execute("SELECT * FROM blogs") 
+myresult = mycursor.fetchall()
+
+for i in myresult:
+    print(i[9]) 
+ 
