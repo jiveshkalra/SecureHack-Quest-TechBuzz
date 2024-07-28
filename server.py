c44e3b7fd692a8fb10febf982b335a5582037c90
Jivesh Kalra <jiveshkalra4@gmail.com>
2024-07-28 19:55:22 +0530

    mysql ocnnection prob fixed

--- server.py
+++ server.py
@@ -1,25 +1,21 @@
 from flask import Flask, redirect, render_template,request
-from mysql_db_config import get_db 
- 
-mydb = get_db()
-app = Flask(__name__) 
- 
-@app.route("/")
-def index(): 
-    mycursor = mydb.cursor()   
-    mycursor.execute("SELECT * FROM blogs") 
-    blogs_data = mycursor.fetchall() 
-    return render_template('index.html', blogs_data=blogs_data)
+import mysql.connector
   
-@app.route("/admin/login")
-def admin_login():
-    # Sessions setup
-    return render_template('admin/login.html')
-
-@app.route("/admin/")
-def admin(): 
-    # Sessions setup
-    return render_template('admin/index.html')
+app = Flask(__name__) 
+mysql_config = { 
+    "host":"sql12.freesqldatabase.com",
+    "user":"sql12722127",
+    "password":"kKVN6xFhI9",
+    "database":"sql12722127",
+    "raise_on_warnings": True
+}
+@app.route("/")
+def index():   
+    with mysql.connector.connect(**mysql_config) as mydb:
+        mycursor = mydb.cursor()
+        mycursor.execute("SELECT * FROM blogs") 
+        blogs_data = mycursor.fetchall() 
+    return render_template('index.html', blogs_data=blogs_data)
 
 
 @app.route("/login")
@@ -29,24 +25,31 @@ def login():
 @app.route("/signup")
 def signup():
     return render_template('signup.html')
-
-@app.route("/adminPage")
-def adminPage():
+  
+@app.route("/admin/login")
+def admin_login(): 
     return render_template('admin/login.html')
 
-@app.route("/adminPageDontComeHereImWarningYou")
-def adminPageDontComeHereImWarningYou():
-    mycursor = mydb.cursor()   
-    mycursor.execute("SELECT * FROM blogs") 
-    blogs_data = mycursor.fetchall() 
-    return render_template("admin/index.html", blogs_data=blogs_data)  
+@app.route("/admin/")
+@app.route("/admin/index")
+def admin(): 
+    return render_template('admin/index.html')
+ 
 
 @app.route("/blogs/<blog_url>")
 def blog(blog_url): 
-    mycursor = mydb.cursor() 
-    mycursor.execute(f"SELECT * FROM blogs WHERE blog_url = '{blog_url}'")
-    blog_data = mycursor.fetchone()   
-    return render_template('blogs.html', blog_data=blog_data ) 
+    try:  
+        with mysql.connector.connect(**mysql_config) as mydb:
+            mycursor = mydb.cursor() 
+            mycursor.execute(f"SELECT * FROM blogs WHERE blog_url = '{blog_url}'")
+            blog_data = mycursor.fetchone() 
+        if blog_data is None:
+            return redirect('/?error_code=blog_not_found')
+        else:
+            return render_template('blogs.html', blog_data=blog_data )
+    except Exception as e:
+        print(e) 
+        return redirect('/?error_code=serverdown')
 
 
 if __name__ == "__main__":
