805a854ee3d4cad87944d9b7b995e15dc6d44f30
Jivesh Kalra <jiveshkalra4@gmail.com>
2024-07-28 16:37:50 +0530

    smtg work in progs

--- server.py
+++ server.py
@@ -10,6 +10,17 @@ def index():
     mycursor.execute("SELECT * FROM blogs") 
     blogs_data = mycursor.fetchall() 
     return render_template('index.html', blogs_data=blogs_data)
+  
+@app.route("/admin/login")
+def admin_login():
+    # Sessions setup
+    return render_template('admin/login.html')
+
+@app.route("/admin/")
+def admin(): 
+    # Sessions setup
+    return render_template('admin/index.html')
+
 
 @app.route("/login")
 def login():
@@ -19,24 +30,14 @@ def login():
 def signup():
     return render_template('signup.html')
 
-@app.route("/adminPageDontComeHereImWarningYou")
-def adminPageDontComeHereImWarningYou():
-    return render_template('admin.html')
-
-@app.route("/adminPage", method=["POST"])
-def adminPage():
-    if request.form.passcode == "Priyanshu is the best coder!!!":
-        return render_template("actualAdmin.html")
-    else:
-        return redirect('/adminPageDontComeHereImWarningYou')
 
 @app.route("/blogs/<blog_url>")
 def blog(blog_url): 
-    mycursor = mydb.cursor()
-    # SELECT * FROM `blogs` WHERE `blog_url` = "typography" 
+    mycursor = mydb.cursor() 
     mycursor.execute(f"SELECT * FROM blogs WHERE blog_url = '{blog_url}'")
     blog_data = mycursor.fetchone()   
     return render_template('blogs.html', blog_data=blog_data ) 
+
 
 if __name__ == "__main__":
     app.run(debug=True)
